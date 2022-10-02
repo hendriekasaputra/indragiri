8bd70237a691378f6411711d9e55b363f230e012
Hendri Eka Saputra <hdr.ekasaputra@gmail.com>
2022-10-02 21:01:30 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,7 +1,7 @@
 // paste in your published Google Sheets URL from the browser address bar
-// var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
 
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Jj4Xnq0ABIzVpEKTIeMyij3ljFoAYgwupZQnH6ussBE/edit#gid=0';
+//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Jj4Xnq0ABIzVpEKTIeMyij3ljFoAYgwupZQnH6ussBE/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 //var googleApiKey = 'AIzaSyD5sZSgkzCXOrUJBWDcOuiU0GB9GpQmI_w';
